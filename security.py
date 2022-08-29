0284f2d8ce948b36cfd7a4e135847c9e90f081a2
hamzacheklat <39276108+hamzacheklat@users.noreply.github.com>
2022-08-30 00:48:12 +0200

    Update security.py

--- security.py
+++ security.py
@@ -1,9 +1,10 @@
 from werkzeug.security import safe_str_cmp
 from models.user import UserModel
+import hmac
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if user and safe_str_cmp(user.password, password):
+    if user and hmac.compare_digest(user.password, password):
         return user
 
 def identity(payload):
