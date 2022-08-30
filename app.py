a40a4ede4a75e6f79f20c8784e0cdeb9cf39f4f0
hamzacheklat <39276108+hamzacheklat@users.noreply.github.com>
2022-08-30 02:07:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,13 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri =  os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+if uri and uri.startswith('postgres://'):
+    uri =uri.replace('postgres://','post           gresql://',1)
+else:
+    uri = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']= uri
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
